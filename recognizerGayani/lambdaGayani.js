24f760d2ea7086ecf6e33ad94e3b5197c1a3eec0
gaya-ekanayaka <gayaninishadika1997@gmail.com>
2019-11-07 13:25:24 +0530

    4

--- recognizerGayani/lambdaGayani.js
+++ recognizerGayani/lambdaGayani.js
@@ -1,22 +1,39 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
     let s3 = event.Records[0].s3;
     rekog.detectLabels({
         Image: {
             S3Object: {
-                Bucket: "gayani.nisha",
+                Bucket: s3.bucket.name,
                 Name: s3.object.key
             }
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            console.log(data);
+            {
+                ddb.put({
+                    TableName: 'gayani',
+                    Item: { 'name': name }
+                }).promise()
+                    .then((data) => {
+                        //your logic goes here
+                    })
+                    .catch((err) => {
+                        //handle error
+                    });
+
+            }
+            let label = data.Labels[0].Name;
+
+
+            /* drag-n-drop DDB table */
+        })
+        .catch(err => callback(err));
 }
